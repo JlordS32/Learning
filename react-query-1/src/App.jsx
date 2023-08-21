92eca01f1be2c57c84841abdb1a1788838b7993c
JlordS32 <jlordsty128@gmail.com>
2023-08-21 13:36:42 +1000

    another commit

--- react-query-1/src/App.jsx
+++ react-query-1/src/App.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<div className='app'>
-			<h1>Hello world</h1>
+		<QueryClientProvider client={queryClient}>
+			<ChildComponent />
+		</QueryClientProvider>
+	);
+}
+
+function ChildComponent() {
+	const fetchCareers = async () => {
+		const res = await fetch('http://careersapi.vercel.app/api/careers');
+		const data = await res.json();
+
+		return data;
+	};
+
+    const fetchCareersById = async (id) => {
+        const res = await fetch(`http://careersapi.vercel.app/api/careers/${id}`);
+        return await res.json();
+    }
+
+	const { status, data, error } = useQuery({
+		queryKey: ['todo'],
+		queryFn: fetchCareers,
+	});
+
+	if (status === 'loading') {
+		console.log('Loading...');
+	}
+
+	if (status === 'error') {
+		return <span>Error: {error.message}</span>;
+	}
+
+	return (
+		<div className='app' style={{
+            fontSize: '9px',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+            gap: '10px',
+        }}>
+			{data?.map((career) => {
+				return (
+					<div key={career.id} style={{
+                        fontFamily: 'Arial, opensans',
+                        backgroundColor: 'gray'
+                    }}>
+						<h1>{career.title}</h1>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<div className='app'>
-			<h1>Hello World</h1>
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<Example />
+		</QueryClientProvider>
 	);
+}
+
+function Example() {
+	const { isFetching, error, data } = useQuery('data', () => {
+		return fetch('https://careersapi.vercel.app/api/careers').then((res) =>
+			res.json()
+		);
+	});
+
+    if (isFetching) return <h1 style={{
+		position: 'absolute',
+		top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+		fontSize: '5rem'
+	}}>Loading...</h1>;
+	if (error) return <p>Error :{error.message}</p>;
+
+    return (
+		data?.map((data) => {
+			return (
+                <div key={data.id}>
+                    <h1>{data.title}</h1>
+					<p>Salary: {data.salary}</p>
+					<h2>Description: </h2>
+                    <p>{data.description}</p>
+                </div>
+            );
+		})
+	)
 }
 
 export default App;
