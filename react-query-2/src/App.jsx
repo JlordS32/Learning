c3f772baf8c5e7d163f27e09180d1e494acdfc3e
JlordS32 <jlordsty128@gmail.com>
2023-08-23 07:50:37 +1000

    changes

--- react-query-2/src/App.jsx
+++ react-query-2/src/App.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+const POSTS = [
+	{
+		id: 1,
+		title: 'Post 1',
+	},
+	{
+		id: 2,
+		title: 'Post 2',
+	},
+];
 
 function App() {
+	const postFn = async () => {};
+
+	const postQuery = useQuery({
+		queryKey: ['post'],
+		queryFn: () => wait(1000).then(() => [...POSTS]),
+	});
+
+	const newPostMutation = useMutation({
+		mutationFn: (title) => {
+			return wait(1000).then(() =>
+				POSTS.push({
+					id: crypto.randomUUID(),
+					title,
+				})
+			);
+		},
+	});
+
+	if (postQuery.isLoading) {
+		return <h1>Loading...</h1>;
+	}
+	if (postQuery.isError) {
+		return <h1>Error: {postQuery.error}</h1>;
+	}
+
 	return (
 		<div className='app'>
-			<h1>Hello from App</h1>
+			{postQuery.data.map((data) => {
+				return (
+					<div key={data.id}>
+						<h1>{data.title}</h1>
+					</div>
+				);
+			})}
 		</div>
 	);
+}
+
+function wait(duration) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, duration);
+	});
 }
 
 export default App;
--- react-query-2/src/main.jsx
+++ react-query-2/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<QueryClientProvider client={client}>
+			<ReactQueryDevtools />
 			<App />
 		</QueryClientProvider>
 	</React.StrictMode>
