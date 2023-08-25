51735480f930bf9867e3f64ff4d84f81547593e2
JlordS32 <jlordsty128@gmail.com>
2023-08-25 10:07:58 +1000

    getting there

--- relearning-js/1.js
+++ relearning-js/1.js
@@ -1,1 +1,13 @@
-console.log('test');
+// let name = null;
+
+// let powerOf = 2 ** 3;
+
+// console.log(powerOf);
+
+// INCREMENT TEST
+
+let num1 = 0;
+let num2 = 0;
+
+console.log(num1++);
+console.log(++num2);
