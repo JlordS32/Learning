8020c49afffa8cc8fda5b70cce1f1171a46dd062
JlordS32 <jlordsty128@gmail.com>
2023-08-29 19:15:19 +1000

    more updates

--- relearning-js/functions.js
+++ relearning-js/functions.js
@@ -1,11 +1,23 @@
-const returnTotalSum = (...args) => {
-	let sum = 0;
+// const returnTotalSum = (...args) => {
+// 	let sum = 0;
 
-	for (let i = 0; i < arguments.length; i++) {
-		sum += args[i];
-	}
+// 	for (let i = 0; i < arguments.length; i++) {
+// 		sum += args[i];
+// 	}
 
-	return sum;
+// 	return sum;
+// };
+
+// console.log(returnTotalSum(3, 34, 3, 42, 23, '23432', true));
+
+// Higher Order Function
+
+const higherOrder = (square, value) => {
+	console.log(`This is the output ${square(value)}`);
 };
 
-console.log(returnTotalSum(3, 34, 3, 42, 23, '23432', true));
+const square = (n) => {
+	return n * n;
+};
+
+higherOrder(square, 5);
